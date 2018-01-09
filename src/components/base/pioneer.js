ee8b231d18b1ca2d9666c83ab0895872ac96b9a1
abe_clark <abeaclark@gmail.com>
2018-01-09 09:01:48 -0700

    make emojis pngs to unify display

--- src/components/base/pioneer.js
+++ src/components/base/pioneer.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import PioneerBoy from 'media/boypioneer.png'
 import PioneerGirl from 'media/pioneergirl.png'
-import Emoji from 'react-emoji-render';
+import Emoji from 'react-emoji-render'
+
+import Cry from 'media/cry.png'
+import Weary from 'media/weary.png'
+import Anguished from 'media/anguished.png'
+import Confused from 'media/confused.png'
+import NeutralFace from 'media/neutral_face.png'
+import Smile from 'media/smile.png'
 
 
 const emojiOptions = [
-  ':cry:',
-  ':anguished:',
-  ':weary:',
-  ':confused:',
-  ':neutral_face:',
-  ':neutral_face:',
-  ':smile:',
-  ':smile:',
+  Cry,
+  Weary,
+  Anguished,
+  Confused,
+  NeutralFace,
+  NeutralFace,
+  Smile,
+  Smile,
 ]
 
 const styles = {
@@ -27,7 +34,7 @@ const Pioneer = ({ gender, scripturesReadCount=0, style={} }) => {
   return (
     <div css={{ ...style, display: 'flex', flexDirection: 'row', fontSize: '10px' }}>
       <div css={{ display: 'flex', flexDirection: 'column' }}>
-        <Emoji style={{ fontSize: '80px', marginBottom: '-20px', zIndex: 3 }} text={emoji} />
+        <img src={emoji} style={{ width: '80px', height: '80px', marginBottom: '-20px', marginLeft: '5px', zIndex: 3 }}/>
         <img src={gender === 'male' ? PioneerBoy : PioneerGirl} css={{ height: '150px' }}/>
       </div>
       <div css={{ width: 200, height: 75, position: 'relative', borderRadius: '50%', zIndex: 3, backgroundColor: 'grey', color: 'white', padding: '20px'}}>
